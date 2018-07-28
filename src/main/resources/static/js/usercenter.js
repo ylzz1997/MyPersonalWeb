605c7d22d853c354881ff3df2f9a9a63b39727c5
ylzz1997 <ylzz1997@qq.com>
2018-07-28 15:40:43 +0800

    #YuriStudio个人网站的源码 没什么可以说的

--- src/main/resources/static/js/usercenter.js
+++ src/main/resources/static/js/usercenter.js
@@ -10,6 +10,24 @@ $(function(){
         }
     });
 
+    $("#btn_email").click(function(){
+        $("#email_gai").css("display","block");
+        $(this).css("display","none");
+    });
 
+    $("#btn_email_close").click(function(){
+        $("#email_gai").css("display","none");
+        $("#btn_email").css("display","inline-block");
+    });
+
+    $("#btn_indexname").click(function(){
+        $("#indexname_gai").css("display","block");
+        $(this).css("display","none");
+    });
+
+    $("#btn_indexname_close").click(function(){
+        $("#indexname_gai").css("display","none");
+        $("#btn_indexname").css("display","inline-block");
+    });
 
 });
